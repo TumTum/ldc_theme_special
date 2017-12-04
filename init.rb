b435716fa03cc4ca933f1bb7dba1957985d45fa9
Tobias Matthaiou <developer@tobimat.eu>
2017-12-04 22:32:52 +0100

    Plugin Struktur

---
+++ init.rb
@@ -0,0 +1,10 @@
+Redmine::Plugin.register :ldc_theme_special do
+  name 'LDC Theme Special '
+  author 'Tobias Matthaiou'
+  description 'Es verändert einwenig die HTML Struktur. z.b. Fügt es die Apple Icons hinzu'
+  version '0.0.1'
+  url 'https://github.com/TumTum/ldc_theme_special'
+  author_url 'http://www.tobimat.eu'
+end
+
+
